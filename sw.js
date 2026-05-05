9685d6459d5663db7a4ae395a2ff339db67c5e9c
Qomaruddin Djamal <jamalpancur@gmail.com>
2026-05-06 02:42:54 +0700

    Update PWA type

--- sw.js
+++ sw.js
@@ -1,43 +1,16 @@
-// Service Worker untuk PWA - Qomaruddin Djamal Portfolio
-const CACHE_NAME = 'qomaruddin-pwa-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  'https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,300;400;500;600;700;800&family=Space+Grotesk:wght@400;500;600;700&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
-];
+const CACHE_NAME = 'djamal-v1';
+const urlsToCache = ['/', '/index.html', '/manifest.json'];
 
-// Install Service Worker
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-// Fetch dari cache (offline support)
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
 
-// Update cache
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
+  event.waitUntil(caches.keys().then(cacheNames => {
+    return Promise.all(cacheNames.map(cache => { if (cache !== CACHE_NAME) return caches.delete(cache); }));
+  }));
 });
